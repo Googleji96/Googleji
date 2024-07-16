c565149955f9f570fbdffd254dcbfb41ccad35f2
Gajendra vishwakarma <gajendravishwakarma004@gmail.com>
2024-07-16 10:14:13 +0530

    New js file

---
+++ index.js
@@ -0,0 +1,48 @@
+console.log("This is a calculator");
+// This is a calculate ------------------------->
+// let random = Math.random();
+// let a = prompt("Entar your first Number");
+// let b = prompt("Entar your Opration Number");
+// let c = prompt("Entar your second  Number");
+
+// if (random > 0.1) {
+//   alert(`This is your result = ${eval(`${a} ${b} ${c}`)}`);
+// }
+
+//--------------------------------------------------------------------------->
+// This is a foulty calculator -------------->
+// Foulty rate of persents 10% ---------------------------->
+
+// let random = Math.random();
+// let a = prompt("Entar your first Number");
+// let b = prompt("Entar your Opration Number");
+// let c = prompt("Entar your second  Number");
+
+// let obj = {
+//   "+": "-",
+//   "*": "+",
+//   "-": "/",
+//   "/": "*",
+// };
+
+// if (random > 0.1) {
+//   alert(`This is your result = ${eval(`${a} ${b} ${c}`)}`);
+// } else {
+//   b = obj[c];
+//   alert(`This is your result = ${eval(`${a} ${b} ${c}`)}`);
+// }
+
+// ------------------------------------------------------------>
+// Resty calculator  por per working ------------------->
+
+let random = Math.random();
+let a = prompt("enter your frist number");
+let b = prompt("enter your Opretor number");
+let c = prompt("enter your second number");
+
+console.log(random);
+
+if (random > 0.1) {
+  alert(`This is your result = ${eval(` ${a} ${b} ${c} `)} `);
+
+}
